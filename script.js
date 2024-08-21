c3f2ede005ac8d0586d399219c082c36e44a42e1
disilvadavis <150249207+disilvadavis@users.noreply.github.com>
2024-08-21 19:38:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let mark = 30
+
+if (mark >= 90) {
+console.log("A+");
+
+} else if (mark >= 80){
+console.log("A");
+
+} else if (mark >= 70){
+console.log("B+");
+
+} else if (mark >= 60){
+console.log("B")
+
+} else if (mark >= 50){
+console.log("C+");
+
+} else {
+console.log("failed");
+}
